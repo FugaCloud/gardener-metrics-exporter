a58db3a510515160843f5e6b13ff29ac2306c792
Thomas Jansema <thomas.jansema@cyso.group>
2023-12-28 13:46:24 +0100

    worker node count and worker type information

--- pkg/metrics/shoot.go
+++ pkg/metrics/shoot.go
@@ -383,6 +383,42 @@ func (c gardenMetricsCollector) collectShootNodeMetrics(shoot *gardenv1beta1.Sho
 			return
 		}
 		ch <- metric
+
+		// Expose metrics. Start with max worker node count.
+		metric, err := prometheus.NewConstMetric(
+			c.descs[metricGardenShootWorkerNodeMaxTotal],
+			prometheus.GaugeValue,
+			float64(worker.Maximum),
+			[]string{
+				shoot.Name,
+				*projectName,
+				worker.Name,
+				worker.Machine.Type,
+			}...,
+		)
+		if err != nil {
+			ScrapeFailures.With(prometheus.Labels{"kind": "shoots"}).Inc()
+			return
+		}
+		ch <- metric
+
+		// Continue with min worker node count.
+		metric, err = prometheus.NewConstMetric(
+			c.descs[metricGardenShootWorkerNodeMinTotal],
+			prometheus.GaugeValue,
+			float64(worker.Minimum),
+			[]string{
+				shoot.Name,
+				*projectName,
+				worker.Name,
+				worker.Machine.Type,
+			}...,
+		)
+		if err != nil {
+			ScrapeFailures.With(prometheus.Labels{"kind": "shoots"}).Inc()
+			return
+		}
+		ch <- metric
 	}
 
 	// Expose metrics. Start with max node count.
